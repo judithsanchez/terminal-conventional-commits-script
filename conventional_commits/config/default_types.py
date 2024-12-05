d5a7e3731259b3267f05c23e597d24fc22c70e4f
Judith Sánchez <judithv.sanchezc@gmail.com>
2024-12-05 15:57:09 +0100

    Create default_types.py

---
+++ conventional_commits/config/default_types.py
@@ -0,0 +1,34 @@
+# Comprehensive list of commit types
+DEFAULT_COMMIT_TYPES = {
+    "feat": "✨",        # A new feature
+    "wip": "🚧",         # Work in progress
+    "fix": "🐛",         # A bug fix
+    "docs": "📝",        # Documentation only changes
+    "style": "💄",       # Code style changes
+    "refactor": "♻️",    # Code refactoring
+    "test": "✅",        # Adding tests
+    "typo": "✏️",        # Fixed a typo
+    "finally": "🎯",     # Finally got something to work
+    "ui": "💅🏻",         # User interface changes
+    "oops": "🤦🏻‍♀️",     # Fixing my own mistake
+    "revert": "⏪️",      # Reverting a commit
+    "init": "🎉",        # Initial commit
+    "debug": "🐞",       # Debugging changes
+    "hotfix": "🔥",      # Critical hotfix
+    "lazy": "💤",        # Shortcut added for convenience
+    "yolo": "🚀",        # Experimental change
+    "ux": "🛋️",          # Improved user experience
+    "perf": "⚡️",       # Code changes that improve performance
+    "style": "💄",       # Changes that do not affect the meaning of the code
+    "merge": "🔀",       # Merge branches
+    "deps": "📦",        # Dependency updates
+    "security": "🔒",    # Security patches or updates
+    "config": "⚙️",      # Configuration file changes
+    "localization": "🌐",# Language localization changes
+    "analytics": "📊",   # Changes related to analytics or tracking
+    "petproject": "🐾",  # A fun personal project
+    "build": "🔧",       # Changes that affect the build system or dependencies
+    "ci": "👷",          # Changes to CI configuration files and scripts
+    "chore": "🗃️",      # Other changes that don't modify src or test files
+    "placeholder": "🚧"  # Added placeholder code or content
+}
